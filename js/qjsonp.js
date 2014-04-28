39dee52f3a5c20bdc0d8b758e66e7138ef4b84b5
Alex <j201.alex@gmail.com>
2014-04-27 23:00:17 -0400

    Delete script after use in qjsonp

--- js/qjsonp.js
+++ js/qjsonp.js
@@ -3,12 +3,14 @@ var q = require('q');
 module.exports = function(url) {
 	var callbackKey = 'qjsonp' + String(Math.random()).slice(2);
 	var deferred = q.defer();
+	var scriptSrc = url.replace('callback=?', 'callback=' + callbackKey);
 	window[callbackKey] = function(text) {
 		deferred.resolve(text);
 		delete window[callbackKey];
+		document.body.removeChild(document.querySelector('script[src="' + scriptSrc + '"]'));
 	};
 	var script = document.createElement('script');
-	script.src = url.replace('callback=?', 'callback=' + callbackKey);
+	script.src = scriptSrc;
 	document.body.appendChild(script);
 	return deferred.promise;
 };
